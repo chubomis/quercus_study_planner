a9aea32689b64f03cc82d00dd1412233fdfcfab4
qureish4 <danial.qureishi@gmail.com>
2026-01-13 02:27:26 -0500

    Got key table data from csc311 front page

--- front_page.py
+++ front_page.py
@@ -1,4 +1,5 @@
 import requests
+from bs4 import BeautifulSoup
 from grab_token import grab_token
 
 BASE = "https://q.utoronto.ca/api/v1"
@@ -17,7 +18,26 @@ if r.status_code != 200:
 
 courses = r.json()
 
-for course in courses:
-    id = course.get("id")
-    name = course.get("name") or course.get("course_code")
-    print(id,  name)
+# for course in courses:
+#     id = course.get("id")
+#     name = course.get("name") or course.get("course_code")
+#     print(id,  name)
+
+csc311_id = 416373
+
+csc311_r = requests.get(f"{BASE}/courses/{csc311_id}/front_page", 
+                        headers=headers)
+csc311_page = csc311_r.json()
+html = csc311_page["body"]
+
+soup = BeautifulSoup(html, "lxml")
+
+table = soup.find("table")
+
+rows = table.find_all("tr")
+
+
+for row in rows:
+    cells = row.find_all("td")
+    cell_text = [cell.get_text(strip=True) for cell in cells]
+    print(cell_text)
